3f8d949828d8b986350c05832f761ee16064e436
Rishabh Mediratta <rishabh@Rishabhs-MacBook-Pro-2.local>
2022-12-03 23:43:51 -0600

    Day 4

---
+++ 4/4_2.py
@@ -0,0 +1,19 @@
+with open("4_1.txt") as f:
+    lines = f.read()
+
+assignments = lines.split("\n")
+
+counter = 0
+
+for curr in assignments:
+    split_str = curr.split("-")
+    num1_1 = int(split_str[0])
+    str23 = split_str[1].split(",")
+    num1_2 = int(str23[0])
+    num2_1 = int(str23[1])
+    num2_2 = int(split_str[2])
+
+    if (num2_1 <= num1_2 and num2_1 >= num1_1) or (num1_1 >= num2_1 and num1_1 <= num2_2):
+        counter += 1
+
+print(counter)
